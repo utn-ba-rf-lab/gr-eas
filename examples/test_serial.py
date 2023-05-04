5012e773a4c9d60baa4d9193b5e1306d942891f0
Marcelo Doallo <m.doallo@frba.utn.edu.ar>
2023-05-03 21:04:38 -0300

    Update test-serial-2.grc file  Cambios a ser confirmados: 	modificados:     examples/test-serial-2.grc

--- examples/test_serial.py
+++ examples/test_serial.py
@@ -22,10 +22,10 @@ if __name__ == '__main__':
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-import sip
-from gnuradio import blocks
-from gnuradio import gr
 from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
+from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
@@ -77,7 +77,7 @@ class test_serial(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.valor = valor = 0
-        self.samp_rate = samp_rate = 8000
+        self.samp_rate = samp_rate = 4000
 
         ##################################################
         # Blocks
@@ -86,49 +86,62 @@ class test_serial(gr.top_block, Qt.QWidget):
         self._valor_win = RangeWidget(self._valor_range, self.set_valor, "Valor", "slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._valor_win)
         self.serializer_serializer_0 = serializer.serializer('/dev/ttyUSB1',True,'detector')
-        self.qtgui_number_sink_0 = qtgui.number_sink(
-            gr.sizeof_float,
-            1,
-            qtgui.NUM_GRAPH_NONE,
-            1,
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1, #number of inputs
             None # parent
         )
-        self.qtgui_number_sink_0.set_update_time(0.1)
-        self.qtgui_number_sink_0.set_title("")
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        units = ['', '', '', '', '',
-            '', '', '', '', '']
-        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
-            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
-        factor = [1, 1, 1, 1, 1,
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
 
         for i in range(1):
-            self.qtgui_number_sink_0.set_min(i, -1)
-            self.qtgui_number_sink_0.set_max(i, 1)
-            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
             if len(labels[i]) == 0:
-                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_number_sink_0.set_label(i, labels[i])
-            self.qtgui_number_sink_0.set_unit(i, units[i])
-            self.qtgui_number_sink_0.set_factor(i, factor[i])
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self.qtgui_number_sink_0.enable_autoscale(False)
-        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
-        self.blocks_vector_source_x_0 = blocks.vector_source_f((1, -1, 1, -1), False, 1, [])
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, .5, valor, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_number_sink_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.serializer_serializer_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.serializer_serializer_0, 0))
 
 
     def closeEvent(self, event):
@@ -144,13 +157,15 @@ class test_serial(gr.top_block, Qt.QWidget):
 
     def set_valor(self, valor):
         self.valor = valor
+        self.analog_sig_source_x_0.set_amplitude(self.valor)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 
@@ -166,6 +181,7 @@ def main(top_block_cls=test_serial, options=None):
 
     tb = top_block_cls()
 
+    tb.start()
 
     tb.show()
 
